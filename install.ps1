2352f18a7d6256eb5c9872808b5cef562dca40b0
Jakub Sikorski <jakubsikorski7@gmail.com>
2012-04-18 00:43:05 +0200

    New install.ps1 script

--- install.ps1
+++ install.ps1
@@ -1,54 +1,117 @@
-Push-Location $PROFILE\..
-$script:installDir 		  = Get-Location
-$script:poshGitDir 		  = "$installDir\Posh-Git"
-$script:copyDir    		  = "$installDir\Workspace scripts"
-$script:configurationFile = "configuration.ps1" 
-Pop-Location
+$script:powerShellDir     	  = Split-Path $PROFILE
+$script:installDir 		  	  = "$powerShellDir\Workspace scripts"
+$script:poshGitDir 		  	  = "$installDir\Posh-Git"
+$script:configurationFile	  = "configuration.ps1"
+$script:configurationFilePath = "$installDir\$configurationFile" 
 
-#Begining installation
-Write-Host
-Write-Host "Begining installation..."
-Write-Host "Instalation directory: $installDir"
-
-#Downloading Posh-Git
-Write-Host
-Write-Host "Downloading Posh-Git..."
-Push-Location $installDir
-git clone https://github.com/dahlbyk/posh-git.git Posh-Git
-Pop-Location
-Write-Host "Posh-Git downloaded."
-
-#Installing Posh-Git (it creates also Powershell profile)
-Write-Host
-Write-Host "Installing Posh-Git..."
-Push-Location $poshGitDir
-.\install.ps1
-Pop-Location
-Write-Host "Posh-Git installed."
-
-#Copying files
-Write-Host
-Write-Host "Copying files..."
-if (!(Test-Path $copyDir))
-{
-	New-Item -Type Directory $copyDir > $null
+function script:Invoke-ErrorCheck($errorMessage) {
+	if (!$?) {
+		Write-Error ($errorMessage + " Installation aborted.")
+		if (Test-Path $installDir) {
+			Remove-Item -Recurse -Force $installDir
+		}
+		exit 1
+	}
 }
-if (!(Test-Path $copyDir\$configurationFile)) {
-	Copy-Item * $copyDir
-}
-else {
-	Copy-Item * $copyDir -Exclude $configurationFile
-}
-Write-Host "Files copied."
 
-#Configuring Powershell profile
-Write-Host
-Write-Host "Configuring Powershell module..."
-$loadLine = "Import-Module '$installDir\Workspace scripts\workspace.psm1'"
-if	(Select-String -Path $PROFILE -Pattern $loadLine -SimpleMatch -Quiet) {
-	Write-Warning "It seems workspace scripts are already installed..."
+function script:Invoke-InstallationStep($step) {
+	Write-Host
+	& $step
 }
-else {
+
+function script:Start-Installation() {
+	Write-Host "Starting installation..."
+	Write-Host "Instalation directory: $installDir"
+}
+
+function script:Remove-OldInstallation() {
+	Write-Host "Removing old installations..."
+	if ((Test-Path $installDir)) {
+		Remove-Item -Recurse -Force -Exclude $configurationFile "$installDir\*"
+		Invoke-ErrorCheck "Cannot create PowerShell directory."	
+		Write-Host "Old installations removed."
+	}
+	else {
+		Write-Host "Old installations were not found."
+	}
+}
+
+function script:Create-PowerShellDirectory() {
+	Write-Host "Creating PowerShell directory..."
+	
+	if (!(Test-Path $powerShellDir)) {
+		New-Item -Type Directory $powerShellDir > $null
+		Invoke-ErrorCheck "Cannot create PowerShell directory."	
+		Write-Host "PowerShell directory created."
+	}
+	else {
+		Write-Host "PowerShell directory already exists."
+	}
+}
+
+function script:Create-InstallationDirectory() {
+	Write-Host "Creating installation directory..."
+	
+	if (!(Test-Path $installDir)) {
+		New-Item -Type Directory $installDir > $null
+		Invoke-ErrorCheck "Cannot create installation directory."	
+		Write-Host "Installation directory created."
+	}
+	else {
+		Write-Host "Installation directory already exists."
+	}
+}
+
+function script:Get-PoshGit() {
+	Write-Host "Downloading Posh-Git..."
+	git clone https://github.com/dahlbyk/posh-git.git $poshGitDir
+	Invoke-ErrorCheck "Cannot download Posh-Git."
+	Write-Host "Posh-Git downloaded."
+}
+
+function script:Invoke-PoshGitInstallation() {
+	Write-Host "Installing Posh-Git..."
+	& "$poshGitDir\install.ps1"
+	Invoke-ErrorCheck "Cannot install Posh-Git."
+	Write-Host "Posh-Git installed."
+}
+
+function script:Copy-Files() {
+	Write-Host "Copying files..."
+
+	if (!(Test-Path $configurationFilePath)) {
+		Copy-Item * $installDir
+	}
+	else {
+		Copy-Item * $installDir -Exclude $configurationFilePath
+		Write-Warning "Old configuration file was detected. It wont be removed..."
+	}
+	Invoke-ErrorCheck "Cannot copy files."
+	
+	Write-Host "Files copied."
+}
+
+function script:Create-PowerShellProfile() {
+	Write-Host "Creating PowerShell profile file..."
+	
+	if (!(Test-Path $PROFILE)) {
+		New-Item -type File $PROFILE > $null
+		Invoke-ErrorCheck "Cannot create PowerShell profile file."
+		Write-Host "PowerShell profile file created."
+	}
+	else {
+		Write-Host "PowerShell profile file already exists."
+	}
+}
+
+function script:Set-Configuration() {
+	Write-Host "Configuring PowerShell module..."
+		
+	$loadLine = "Import-Module '$installDir\workspace.psm1'"
+	if	(Select-String -Path $PROFILE -Pattern $loadLine -SimpleMatch -Quiet) {
+		Write-Warning "It seems workspace scripts were already installed..."
+	}
+	else {
 @"
 
 # Load workspace scripts
@@ -57,11 +120,25 @@ $loadLine
 # Start Pageant
 Start-Pageant
 "@ | Out-File $PROFILE -Append -Encoding utf8
+		Invoke-ErrorCheck "Cannot configure PowerShell module."
+	}
+	
+	Write-Host "Poweshell module configured."
 }
 
-Write-Host "Poweshell module configured."
+function script:Stop-Installation() {
+	Write-Host "Installation ended."
+	Write-Host
+}
 
-#Ending installation
-Write-Host
-Write-Host "Installation ended."
-Write-Host
+#Installation
+Invoke-InstallationStep("Start-Installation")
+Invoke-InstallationStep("Remove-OldInstallation")
+Invoke-InstallationStep("Create-PowerShellDirectory")
+Invoke-InstallationStep("Create-InstallationDirectory")
+Invoke-InstallationStep("Get-PoshGit")
+Invoke-InstallationStep("Invoke-PoshGitInstallation")
+Invoke-InstallationStep("Copy-Files")
+Invoke-InstallationStep("Create-PowerShellProfile")
+Invoke-InstallationStep("Set-Configuration")
+Invoke-InstallationStep("Stop-Installation")
